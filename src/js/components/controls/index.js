9d55583f5e74be05ef353d04b6d19967d9c175b0
StetsD <chortstd@gmail.com>
2017-10-27 18:55:37 +0300

    check

--- src/js/components/controls/index.js
+++ src/js/components/controls/index.js
@@ -5,6 +5,14 @@ import {Form, Radio} from 'semantic-ui-react';
 export default class Controls extends Component{
     constructor(props){
         super(props);
+        this.handleChangeMode = this.handleChangeMode.bind(this);
+        this.state = {
+            valueMode: 'timer'
+        }
+    }
+
+    handleChangeMode(e, {value}){
+        this.setState({valueMode: value});
     }
 
     render(){
@@ -12,13 +20,13 @@ export default class Controls extends Component{
             <Form>
                 <Form.Field>Select mode</Form.Field>
                 <Form.Field>
-                    <Radio label="Timer" name="appMode" value="timer"></Radio>
+                    <Radio label="Timer" onChange={this.handleChangeMode} checked={this.state.valueMode === 'timer'} name="appMode" value="timer"></Radio>
                 </Form.Field>
                 <Form.Field>
-                    <Radio label="Timer-Chain" name="appMode" value="timer-chain"></Radio>
+                    <Radio label="Timer-Chain" onChange={this.handleChangeMode} checked={this.state.valueMode === 'timer-chain'} name="appMode" value="timer-chain"></Radio>
                 </Form.Field>
                 <Form.Field>
-                    <Radio label="Stopwatch" name="appMode" value="stopwatch"></Radio>
+                    <Radio label="Stopwatch" onChange={this.handleChangeMode} checked={this.state.valueMode === 'stopwatch'} name="appMode" value="stopwatch"></Radio>
                 </Form.Field>
             </Form>
         )
